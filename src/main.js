6a3d5063bd7eff64eb3f56a104fcfade67ad7178
lionCL <18688788437@163.com>
2019-10-03 00:35:32 +0800

    路由文件配置
    下载创建导入路由文件  . 下载使用element-ui插件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+//加载element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+//导入路由
+import router from '@/router/'
+
+//使用element
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  //挂在路由
+  router
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+//创建规则
+const routes = []
+
+//创建路由实例
+const router = new VueRouter({
+  routes
+})
+
+//暴露
+export default router
