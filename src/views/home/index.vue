964eeb36f1ca4d21b7419caf785a85384743e449
lionCL <18688788437@163.com>
2019-10-04 22:10:06 +0800

    首页 -  头部页面完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,27 @@
 <template>
   <el-container class="container">
-    <el-header class="header">Header</el-header>
+    <!-- 头部 -->
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4">
+          <img src="@/assets/imgs/logo.png"
+               alt=""
+               class="logo">
+        </el-col>
+        <el-col :span="16">
+          <div class="title">电商后台管理系统</div>
+        </el-col>
+        <el-col :span="4">
+          <a href="#"
+             class="loginout">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
+      <!-- 侧栏 -->
       <el-aside class="aside"
                 width="200px">Aside</el-aside>
+      <!-- main -->
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
@@ -19,6 +37,24 @@ export default {
   height: 100%;
   .header {
     background-color: #b3c0d1;
+    padding: 0;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+    .logo {
+      margin-right: 40px;
+      height: 50px;
+    }
+    .title {
+      color: #fff;
+      font-size: 24px;
+    }
+    .loginout {
+      position: absolute;
+      right: 20px;
+      font-size: 14px;
+      color: orange;
+    }
   }
   .aside {
     background-color: #d3dce6;
